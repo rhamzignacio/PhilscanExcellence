3ccc65fce24bc4b97ee03beaee274ffb65565b1e
Royce Stephan Ignacio <ramil.charles.ignacio@gmail.com>
2017-11-28 10:45:06 +0800

    initial commit

---
+++ PhilscanExcellence/PhilscanExcellence/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhilscanExcellence.Startup))]
+namespace PhilscanExcellence
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
